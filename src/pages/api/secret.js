54d8f85448c4189b3efd1a8b8e2cb4b685af4ad7
Daniel Mbengui <danielmbengui@Ordinateur-portable-de-Daniel.local>
2024-12-02 23:55:24 +0100

    start to create api to get secret keys corret

--- src/pages/api/secret.js
+++ src/pages/api/secret.js
@@ -1,6 +1,5 @@
 import { SecretManagerServiceClient } from '@google-cloud/secret-manager';
 require('dotenv').config();
-
 const client = new SecretManagerServiceClient({
   credentials: {
     private_key: process.env.GCP_PRIVATE_KEY.replace(/\\n/g, '\n'),
