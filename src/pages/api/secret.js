fb3d5b047ebd52185d067431838102c7dbd5ea88
Daniel Mbengui <danielmbengui@Ordinateur-portable-de-Daniel.local>
2024-12-03 00:29:38 +0100

    start to create api to get secret keys corret with new key

--- src/pages/api/secret.js
+++ src/pages/api/secret.js
@@ -17,17 +17,13 @@ export default async function handler(req, res) {
   try {
     const secretName = 'VLSJINHIeATv4nVi7O4Y'; // Remplacez par votre nom de secret
     const version = '27';
-    const name = `projects/${process.env.GCP_PROJECT_ID}/secrets/${secretName}/versions/${version}`;
+    const name = `projects/${"839974397439"}/secrets/${secretName}/versions/${version}`;
     
     const [accessResponse] = await client.accessSecretVersion({ name });
     const secretValue = accessResponse.payload.data.toString('utf8');
     const values = JSON.parse(secretValue);
     const clubUid = values.clubUid;
-    res.status(200).json({
-      apiKey: process.env.GCP_PRIVATE_KEY,
-      projectId: process.env.GCP_CLIENT_EMAIL,
-      secretKey: process.env.GCP_PROJECT_ID ? 'Loaded' : 'Missing',
-    });
+    res.status(200).json({ clubUid: clubUid});
   } catch (error) {
     console.error('Erreur lors de la récupération du secret :', error);
     res.status(500).json({ error: 'Erreur lors de la récupération du secret' });
