cfdfbb6f025c35e8e0affb8307e62df9a88e7492
Daniel Mbengui <danielmbengui@ordinatdedaniel.home>
2024-12-03 19:55:47 +0100

    test calendar create smart padel booking

--- src/pages/api/debug.js
+++ src/pages/api/debug.js
@@ -1,6 +1,6 @@
 export default function handler(req, res) {
     res.status(200).json({
-      apiKey: process.env.GCP_PRIVATE_KEY,
+      apiKey: process.env.NEXT_PUBLIC_WEBSITE_LINK,
       projectId: process.env.GCP_CLIENT_EMAIL,
       secretKey: process.env.GCP_PROJECT_ID ? 'Loaded' : 'Missing',
     });
