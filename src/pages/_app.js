61ef9f9af5eb2dd0f251cedfb48af7f555e9cf19
Daniel Mbengui <danielmbengui@ordinatdedaniel.home>
2024-12-06 07:52:23 +0100

    go to last version before hack

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -13,13 +13,11 @@ import { useEffect, useState } from "react";
 export default function App({ Component, pageProps }) {
   //const { theme } = useTheme(); // Récupérer le thème clair/sombre
   //const muiTheme = getMuiTheme('light'); // Générer le thème MUI basé sur vos variables CSS
-  return (<SpeedInsights>
-    <ThemeProvider>
-      <DevLinkProvider>
-        <AuthProvider>
-          <Component remoteConfig={remoteConfig} {...pageProps} />
-        </AuthProvider>
-      </DevLinkProvider>
-    </ThemeProvider>
-  </SpeedInsights>);
+  return (    <ThemeProvider>
+    <DevLinkProvider>
+      <AuthProvider>
+        <Component remoteConfig={remoteConfig} {...pageProps} />
+      </AuthProvider>
+    </DevLinkProvider>
+  </ThemeProvider>);
 }
