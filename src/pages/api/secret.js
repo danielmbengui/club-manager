62e7a720736217eb7369c2f499eab476a87f1ad8
Daniel Mbengui <danielmbengui@ordinatdedaniel.home>
2024-12-06 07:27:32 +0100

    go to last version before hack

--- src/pages/api/secret.js
+++ src/pages/api/secret.js
@@ -8,3 +8,25 @@ const client = new SecretManagerServiceClient({
   },
   projectId: process.env.GCP_PROJECT_ID,
 });
+
+export default async function handler(req, res) {
+  if (req.method !== 'GET') {
+    res.status(405).json({ error: 'Méthode non autorisée' });
+    return;
+  }
+
+  try {
+    const secretName = 'VLSJINHIeATv4nVi7O4Y'; // Remplacez par votre nom de secret
+    const version = '27';
+    const name = `projects/${process.env.GCP_PROJECT_ID}/secrets/${secretName}/versions/${version}`;
+    
+    const [accessResponse] = await client.accessSecretVersion({ name });
+    const secretValue = accessResponse.payload.data.toString('utf8');
+    const values = JSON.parse(secretValue);
+    const clubUid = values.clubUid;
+    res.status(200).json({ clubUid: clubUid});
+  } catch (error) {
+    console.error('Erreur lors de la récupération du secret :', error);
+    res.status(500).json({ error: 'Erreur lors de la récupération du secret' });
+  }
+}
